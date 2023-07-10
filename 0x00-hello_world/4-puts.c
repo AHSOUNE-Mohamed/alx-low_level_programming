43fae0a699bff4020de6c8391a8865bc41752aee
AHSOUNE Mohamed <Ahsoune.mohamed@gmail.com>
2023-07-10 07:03:58 -0700

    recurision

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main a program that print a line
- */
+*main-a program that print a line in one function
+*Return:0(Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main program printf ....
- */
-int main(void);
+*main - a program that print a line with printf function
+*Return: 0 (success)
+*/
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
